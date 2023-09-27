be18bc43e9f9d0801bf282c25088d830db5b0fa2
aryan <aryan@192.168.2.20>
2023-09-26 23:56:21 -0400

    LUCKY CARD

--- src/card/CardTrick.java
+++ src/card/CardTrick.java
@@ -1,15 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package card;
-
-/**
- * A class that fills a magic hand of 7 cards with random Card Objects
- * and then asks the user to pick a card and searches the array of cards
- * for the match to the user's card. To be used as starting code in ICE 1
- * @author srinivsi
- */
 
 import java.util.Random;
 import java.util.Scanner;
